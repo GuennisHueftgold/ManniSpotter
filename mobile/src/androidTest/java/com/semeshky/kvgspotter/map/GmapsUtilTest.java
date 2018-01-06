b19de5513185b6cf608a85085537a06e305727b5
GuennisHueftgold <34449444+GuennisHueftgold@users.noreply.github.com>
2018-01-06 18:42:13 +0100

    Next test

--- mobile/src/androidTest/java/com/semeshky/kvgspotter/map/GmapsUtilTest.java
+++ mobile/src/androidTest/java/com/semeshky/kvgspotter/map/GmapsUtilTest.java
@@ -72,4 +72,37 @@ public class GmapsUtilTest {
                 .append(latLngInterface.getLongitude() / TrapezeApiClient.COORDINATES_CONVERTION_CONSTANT);
         assertEquals(Uri.parse(stringBuilder.toString()), intent.getData());
     }
+
+    @Test
+    public void openNavigationTo_should_call_correctly_to_open_gmaps() throws PackageManager.NameNotFoundException {
+        final MockContext mockContext = mock(MockContext.class);
+        final PackageManager mockPackageManager = mock(PackageManager.class);
+        when(mockPackageManager.getPackageInfo(ArgumentMatchers.anyString(), ArgumentMatchers.anyInt()))
+                .thenReturn(null);
+        when(mockContext.getPackageManager()).thenReturn(mockPackageManager);
+        final LatLngInterface latLngInterface = new LatLngInterface() {
+            @Override
+            public long getLongitude() {
+                return 10;
+            }
+
+            @Override
+            public long getLatitude() {
+                return 20;
+            }
+        };
+        GMapsUtil.openNavigationTo(mockContext,
+                latLngInterface);
+        ArgumentCaptor<Intent> argument = ArgumentCaptor.forClass(Intent.class);
+        verify(mockContext, times(1)).startActivity(argument.capture());
+        final Intent intent = argument.getValue();
+        assertEquals(intent.getAction(), Intent.ACTION_VIEW);
+        StringBuilder stringBuilder = new StringBuilder()
+                .append("google.navigation:q=")
+                .append(latLngInterface.getLatitude() / TrapezeApiClient.COORDINATES_CONVERTION_CONSTANT)
+                .append(",")
+                .append(latLngInterface.getLongitude() / TrapezeApiClient.COORDINATES_CONVERTION_CONSTANT);
+        assertEquals("com.google.android.apps.maps", intent.getPackage());
+        assertEquals(Uri.parse(stringBuilder.toString()), intent.getData());
+    }
 }
