4a1ca77c39998dbb3a14c22b0f06321b388e5a86
GuennisHueftgold <34449444+GuennisHueftgold@users.noreply.github.com>
2018-01-17 08:30:07 +0100

    AbstractLocationHelperTests

--- mobile/src/test/java/com/semeshky/kvgspotter/adapter/AbstractDataboundAdapterDiffUtilCallbackTest.java
+++ mobile/src/test/java/com/semeshky/kvgspotter/adapter/AbstractDataboundAdapterDiffUtilCallbackTest.java
@@ -8,7 +8,6 @@ import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.mockito.ArgumentMatchers.any;
@@ -24,7 +23,6 @@ public class AbstractDataboundAdapterDiffUtilCallbackTest {
     @Test
     public void areItemsTheSame_should_work_properly() {
         final AbstractDataboundAdapter mockAdapter = mock(AbstractDataboundAdapter.class);
-        final List<Departure> itemList = new ArrayList<>();
         final List<Departure> itemListSpy = mock(List.class);
         final Departure mockDeparture = mock(Departure.class);
         when(itemListSpy.get(anyInt())).thenReturn(mockDeparture);
@@ -33,6 +31,21 @@ public class AbstractDataboundAdapterDiffUtilCallbackTest {
         AbstractDataboundAdapterDiffUtilCallback callback = new AbstractDataboundAdapterDiffUtilCallback(mockAdapter, itemListSpy);
         callback.areItemsTheSame(10, 10);
         verify(mockAdapter, times(1)).areItemsTheSame(mockDeparture, mockDeparture);
+        verify(itemListSpy, times(1)).get(10);
+        verify(mockAdapter, times(1)).getItem(10);
+    }
+
+    @Test
+    public void areContentsTheSame_should_work_properly() {
+        final AbstractDataboundAdapter mockAdapter = mock(AbstractDataboundAdapter.class);
+        final List<Departure> itemListSpy = mock(List.class);
+        final Departure mockDeparture = mock(Departure.class);
+        when(itemListSpy.get(anyInt())).thenReturn(mockDeparture);
+        when(mockAdapter.getItem(anyInt())).thenReturn(mockDeparture);
+        when(mockAdapter.areContentsTheSame(any(), any())).thenReturn(true);
+        AbstractDataboundAdapterDiffUtilCallback callback = new AbstractDataboundAdapterDiffUtilCallback(mockAdapter, itemListSpy);
+        callback.areContentsTheSame(10, 10);
+        verify(mockAdapter, times(1)).areContentsTheSame(mockDeparture, mockDeparture);
         verify(itemListSpy, times(1)).get(10);
         verify(mockAdapter, times(1)).getItem(10);
     }
