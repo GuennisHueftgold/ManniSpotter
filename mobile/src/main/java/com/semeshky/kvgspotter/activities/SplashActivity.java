5a235e54eb054f97923034c958270c4f365b0f95
GuennisHueftgold <34449444+GuennisHueftgold@users.noreply.github.com>
2018-01-21 19:58:33 +0100

    splashscreen update

--- mobile/src/main/java/com/semeshky/kvgspotter/activities/SplashActivity.java
+++ mobile/src/main/java/com/semeshky/kvgspotter/activities/SplashActivity.java
@@ -7,6 +7,9 @@ import android.support.v7.app.AppCompatActivity;
 
 import com.semeshky.kvgspotter.viewmodel.SplashActivityViewModel;
 
+/**
+ * Activity to be used as splashscreen on app start to display app logo
+ */
 public final class SplashActivity extends AppCompatActivity {
     private SplashActivityViewModel mViewModel;
 
