ac3b9e155cfa5c60eb773ccd21d877c377659b4a
GuennisHueftgold <34449444+GuennisHueftgold@users.noreply.github.com>
2018-01-26 09:01:08 +0100

    SplashActivity first setup

--- mobile/src/main/java/com/semeshky/kvgspotter/activities/SplashActivity.java
+++ mobile/src/main/java/com/semeshky/kvgspotter/activities/SplashActivity.java
@@ -1,33 +1,93 @@
 package com.semeshky.kvgspotter.activities;
 
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.constraint.ConstraintLayout;
+import android.support.constraint.ConstraintSet;
 import android.support.transition.ChangeBounds;
 import android.support.transition.Fade;
-import android.support.transition.Scene;
 import android.support.transition.Transition;
 import android.support.transition.TransitionManager;
 import android.support.transition.TransitionSet;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.semeshky.kvgspotter.R;
 import com.semeshky.kvgspotter.settings.ClientSettings;
 import com.semeshky.kvgspotter.viewmodel.SplashActivityViewModel;
 
-import java.util.concurrent.TimeUnit;
-
-import io.reactivex.Single;
-import io.reactivex.disposables.Disposable;
-import io.reactivex.observers.DisposableSingleObserver;
-
 /**
  * Activity to be used as splashscreen on app start to display app logo
  */
 public final class SplashActivity extends AppCompatActivity {
+    private static final String KEY_ANIMATION_PLAYED = SplashActivity.class.getSimpleName() + ".animation_played";
     private SplashActivityViewModel mViewModel;
+    protected final View.OnClickListener mOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            if (view.getId() == R.id.btnSynchronize) {
+                if (mViewModel.isSynchronized()) {
+                    ClientSettings
+                            .getInstance(SplashActivity.this)
+                            .setFirstSetup(true);
+                    gotoMainActivity();
+                } else {
+                    SplashActivity
+                            .this
+                            .mViewModel
+                            .synchronize();
+                }
+            }
+        }
+    };
     private ConstraintLayout mConstraintLayout;
-    private Disposable mUpdateDisposable;
+    private boolean state1 = false, state2 = false;
+    private boolean mAnimationPlayed = false;
+
+    protected void startInAnimation() {
+        ConstraintSet constraintSet2 = new ConstraintSet();
+        constraintSet2.clone(SplashActivity.this, R.layout.activity_splash_setup_end);
+        //////////
+        Transition transitionScale = new ChangeBounds();
+        transitionScale.addTarget(R.id.appIcon);
+        Transition transitionFade = new Fade();
+        transitionFade.addTarget(R.id.btnSynchronize);
+        transitionFade.addTarget(R.id.progressBar);
+        transitionFade.addTarget(R.id.txtStatus);
+        transitionFade.addTarget(R.id.txtTitle);
+        transitionFade.addTarget(R.id.txtFirstSetupDescription);
+        TransitionSet transitionSet = new TransitionSet();
+        transitionSet.setOrdering(TransitionSet.ORDERING_SEQUENTIAL);
+        transitionSet.addTransition(transitionScale);
+        transitionSet.addTransition(transitionFade);
+        transitionSet.setStartDelay(100);
+        //transitionSet.setDuration(10000);
+        //////////
+        TransitionManager.beginDelayedTransition(mConstraintLayout, transitionSet);
+        constraintSet2.applyTo(mConstraintLayout);
+    }
+
+    @Override
+    public void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        state1 = true;
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean focus) {
+        super.onWindowFocusChanged(focus);
+        startInAnimation();
+        if (state1 && state2)
+            return;
+        state2 = true;
+        startInAnimation();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,9 +96,54 @@ public final class SplashActivity extends AppCompatActivity {
                 .isFirstSetupDone()) {
             gotoMainActivity();
         } else {
+            if (savedInstanceState != null)
+                this.mAnimationPlayed = savedInstanceState.getBoolean(KEY_ANIMATION_PLAYED, false);
+            else
+                this.mAnimationPlayed = false;
             this.setTheme(R.style.AppTheme);
-            this.setContentView(R.layout.activity_splash_setup);
+            this.setContentView(this.mAnimationPlayed ? R.layout.activity_splash_setup_end : R.layout.activity_splash_setup);
             this.mConstraintLayout = this.findViewById(R.id.constraintLayout);
+            this.mViewModel = ViewModelProviders.of(this).get(SplashActivityViewModel.class);
+            this.mViewModel.getSyncStatus()
+                    .observe(this, new Observer<Integer>() {
+                        @Override
+                        public void onChanged(@Nullable Integer status) {
+                            if (status == null)
+                                return;
+                            updateStatus(status);
+                        }
+                    });
+            this.findViewById(R.id.btnSynchronize)
+                    .setOnClickListener(this.mOnClickListener);
+        }
+    }
+
+    protected void updateStatus(@SplashActivityViewModel.SyncStatus int status) {
+        TransitionManager.beginDelayedTransition((ViewGroup) findViewById(R.id.constraintLayout));
+        findViewById(R.id.progressBar)
+                .setVisibility(status == SplashActivityViewModel.SYNC_STATUS_SYNCHRONIZING ? View.VISIBLE : View.GONE);
+        final Button syncButton = findViewById(R.id.btnSynchronize);
+        if (status == SplashActivityViewModel.SYNC_STATUS_SYNCHRONIZING) {
+            syncButton.setEnabled(false);
+            syncButton.setText(R.string.synchronizing_ellipsis);
+        } else if (status == SplashActivityViewModel.SYNC_STATUS_SYNCED) {
+            syncButton.setEnabled(true);
+            syncButton.setText(R.string.proceed);
+        } else {
+            syncButton.setEnabled(true);
+            syncButton.setText(R.string.synchronize);
+        }
+        final TextView txtStatus = findViewById(R.id.txtStatus);
+        if (status == SplashActivityViewModel.SYNC_STATUS_SYNCED) {
+            txtStatus.setText(R.string.successfully_synchronized);
+            txtStatus.setTextColor(getResources().getColor(R.color.success_green));
+            txtStatus.setVisibility(View.VISIBLE);
+        } else if (status == SplashActivityViewModel.SYNC_STATUS_ERROR) {
+            txtStatus.setVisibility(View.VISIBLE);
+            txtStatus.setTextColor(getResources().getColor(R.color.error_red));
+            txtStatus.setText(R.string.an_error_occured_check_internet);
+        } else {
+            txtStatus.setVisibility(View.GONE);
         }
     }
 
@@ -52,42 +157,28 @@ public final class SplashActivity extends AppCompatActivity {
     public void onResume() {
         super.onResume();
         if (this.mConstraintLayout != null) {
-
-            Transition transitionScale = new ChangeBounds();
-            transitionScale.addTarget(R.id.progressBar);
-            transitionScale.addTarget(R.id.txtStatus);
-            transitionScale.addTarget(R.id.appIcon);
-            Transition transitionFade = new Fade();
-            transitionFade.addTarget(R.id.progressBar);
-            transitionFade.addTarget(R.id.txtStatus);
-            TransitionSet transitionSet = new TransitionSet();
-            transitionSet.setOrdering(TransitionSet.ORDERING_TOGETHER);
-            transitionSet.addTransition(transitionScale);
-            transitionSet.addTransition(transitionFade);
-
-            final Scene scene = Scene.getSceneForLayout(mConstraintLayout,
-                    R.layout.activity_splash_setup_end, SplashActivity.this);
-            TransitionManager.go(scene, transitionSet);
-            //TODO Lock rotation so it won't reset
-            Single.timer(2, TimeUnit.SECONDS)
-                    .subscribe(new DisposableSingleObserver<Long>() {
-                        @Override
-                        public void onSuccess(Long aLong) {
-                            gotoMainActivity();
-                        }
-
-                        @Override
-                        public void onError(Throwable e) {
-
-                        }
-                    });
+            if (!this.mAnimationPlayed) {
+                this.mAnimationPlayed = true;
+                this.mConstraintLayout.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        startInAnimation();
+                    }
+                });
+            }
         }
     }
 
     @Override
-    public void onPause() {
-        super.onPause();
-        if (this.mUpdateDisposable != null)
-            this.mUpdateDisposable.dispose();
+    public void onRestoreInstanceState(Bundle state) {
+        super.onRestoreInstanceState(state);
+        this.mAnimationPlayed = state.getBoolean(KEY_ANIMATION_PLAYED, false);
     }
+
+    @Override
+    public void onSaveInstanceState(Bundle state) {
+        super.onSaveInstanceState(state);
+        state.putBoolean(KEY_ANIMATION_PLAYED, this.mAnimationPlayed);
+    }
+
 }
--- mobile/src/main/java/com/semeshky/kvgspotter/viewmodel/SplashActivityViewModel.java
+++ mobile/src/main/java/com/semeshky/kvgspotter/viewmodel/SplashActivityViewModel.java
@@ -1,22 +1,105 @@
 package com.semeshky.kvgspotter.viewmodel;
 
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.MutableLiveData;
 import android.arch.lifecycle.ViewModel;
+import android.support.annotation.IntDef;
 
 import com.semeshky.kvgspotter.database.DatabaseSynchronizer;
 
+import java.lang.annotation.Retention;
+import java.util.concurrent.atomic.AtomicBoolean;
+
 import io.reactivex.Single;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.functions.BiFunction;
+import io.reactivex.observers.DisposableSingleObserver;
+import io.reactivex.schedulers.Schedulers;
+
+import static java.lang.annotation.RetentionPolicy.SOURCE;
 
 /**
  * ViewModel for {@link com.semeshky.kvgspotter.activities.SplashActivity}
  */
 public class SplashActivityViewModel extends ViewModel {
-    /**
-     * Checks if the data is synchronized for the first start up
-     *
-     * @return
-     */
-    public Single<Boolean> isSynchronized() {
-        return DatabaseSynchronizer
-                .isDataSynchronized();
+    public final static int SYNC_STATUS_SYNCED = 1,
+            SYNC_STATUS_ERROR = 2,
+            SYNC_STATUS_NOT_SYNCED = 3,
+            SYNC_STATUS_SYNCHRONIZING = 4;
+
+    private final MutableLiveData<Integer> mSyncStatus = new MutableLiveData<>();
+
+    private final AtomicBoolean mSynchronizing = new AtomicBoolean(false);
+    private final AtomicBoolean mSynchronized = new AtomicBoolean(false);
+
+    public SplashActivityViewModel() {
+        this.mSyncStatus.setValue(SYNC_STATUS_NOT_SYNCED);
+    }
+
+    public boolean isSynchronizing() {
+        return this.mSynchronizing.get();
+    }
+
+    public boolean getSynchronized() {
+        return this.mSynchronized.get();
+    }
+
+    public void synchronize() {
+        if (this.mSynchronized.get()) {
+            return;
+        }
+        if (!this.mSynchronizing.compareAndSet(false, true)) {
+            return;
+        }
+        this.setSyncStatus(SYNC_STATUS_SYNCHRONIZING);
+        Single.zip(DatabaseSynchronizer.synchronizeStops(),
+                DatabaseSynchronizer.synchronizeStopPoints(),
+                new BiFunction<Integer, Integer, Integer>() {
+                    @Override
+                    public Integer apply(Integer integer, Integer integer2) throws Exception {
+                        return integer + integer2;
+                    }
+                })
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new DisposableSingleObserver<Integer>() {
+                    @Override
+                    public void onSuccess(Integer integer) {
+                        SplashActivityViewModel
+                                .this
+                                .setSyncStatus(SYNC_STATUS_SYNCED);
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        SplashActivityViewModel
+                                .this
+                                .setSyncStatus(SYNC_STATUS_ERROR);
+                    }
+                });
+    }
+
+    public LiveData<Integer> getSyncStatus() {
+        return mSyncStatus;
+    }
+
+    protected void setSyncStatus(@SyncStatus int status) {
+        if (status == SYNC_STATUS_SYNCED) {
+            this.mSynchronized.set(true);
+        } else {
+            this.mSynchronized.set(false);
+        }
+        this.mSynchronizing.set(false);
+        this.mSyncStatus
+                .postValue(status);
+    }
+
+    public boolean isSynchronized() {
+        return this.mSynchronized.get();
+    }
+
+    @Retention(SOURCE)
+    @IntDef({SYNC_STATUS_SYNCED, SYNC_STATUS_ERROR, SYNC_STATUS_NOT_SYNCED, SYNC_STATUS_SYNCHRONIZING})
+    public @interface SyncStatus {
     }
 }
