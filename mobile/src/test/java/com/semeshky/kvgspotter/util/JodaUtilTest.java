9da74bf82c42c824c66fc35eeb8d64320b32a7b8
GuennisHueftgold <34449444+GuennisHueftgold@users.noreply.github.com>
2018-02-07 00:47:52 +0100

    JodaUtilTest for date skips

--- mobile/src/test/java/com/semeshky/kvgspotter/util/JodaUtilTest.java
+++ mobile/src/test/java/com/semeshky/kvgspotter/util/JodaUtilTest.java
@@ -5,26 +5,38 @@ import android.content.Context;
 import com.semeshky.kvgspotter.BuildConfig;
 import com.semeshky.kvgspotter.R;
 
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeUtils;
 import org.joda.time.LocalTime;
 import org.joda.time.format.DateTimeFormat;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.RuntimeEnvironment;
 import org.robolectric.annotation.Config;
+import org.robolectric.shadows.ShadowSystemClock;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(RobolectricTestRunner.class)
-@Config(constants = BuildConfig.class)
+@Config(constants = BuildConfig.class, shadows = {ShadowSystemClock.class})
 public class JodaUtilTest {
 
+    private final DateTime mSystemDateTime = new DateTime(2010, 6, 6, 23, 50, 0, 0);
     private Context context;
 
     @Before
     public void setup() {
         context = RuntimeEnvironment.application;
+        DateTimeUtils.setCurrentMillisFixed(this.mSystemDateTime.getMillis());
+    }
+
+    @After
+    public void dismantle() {
+        DateTimeUtils.setCurrentMillisSystem();
     }
 
     @Test()
@@ -59,5 +71,19 @@ public class JodaUtilTest {
         final LocalTime localTime = LocalTime.now().plusMinutes(50);
         final String result = JodaUtil.convertLocalTimeOrDelta(context, localTime);
         assertEquals(localTime.toString(DateTimeFormat.shortDateTime()), result);
+    }
+
+    @Test()
+    public void convertLocalTimeOrDelta_should_provide_correct_time_for_day_skip() {
+        final DateTime currentTime = new DateTime(2010, 6, 6, 23, 50, 0, 0);
+        assertTrue(ShadowSystemClock.setCurrentTimeMillis(currentTime.getMillis()));
+
+        final LocalTime localTime = new LocalTime(0, 6, 0, 0);
+        final String result = JodaUtil.convertLocalTimeOrDelta(context, localTime);
+        assertEquals(context.getResources().getQuantityString(R.plurals.minutes, 16), result);
+
+        final LocalTime localTime2 = new LocalTime(23, 56, 0, 0);
+        final String result2 = JodaUtil.convertLocalTimeOrDelta(context, localTime2);
+        assertEquals(context.getResources().getQuantityString(R.plurals.minutes, 6), result2);
     }
 }
